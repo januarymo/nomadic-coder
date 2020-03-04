e511c78c7f02a65f61f774b2a72a81fb827973ed
Brandon Douglas Johnson <brandojd93@gmail.com>
2020-03-03 19:02:53 -0600

    map almost done

--- app/controllers/tutor_profiles_controller.rb
+++ app/controllers/tutor_profiles_controller.rb
@@ -6,7 +6,27 @@ class TutorProfilesController < ApplicationController
         lat: tutor_profile.latitude,
         lng: tutor_profile.longitude,
         infoWindow: render_to_string(partial: "info_window", locals: { tutor_profile: tutor_profile })
+        image_url: helpers.asset_url('')
       }
+
+  markers.forEach((marker) => {
+
+  const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+  const element = document.createElement('div');
+  element.className = 'marker';
+  element.style.backgroundImage = `url('${marker.image_url}')`;
+  element.style.backgroundSize = 'contain';
+  element.style.width = '25px';
+  element.style.height = '25px';
+
+  new mapboxgl.Marker(element)
+    .setLngLat([marker.lng, marker.lat])
+    .setPopup(popup)
+    .addTo(map);
+});
+
     end
   end
 end
+

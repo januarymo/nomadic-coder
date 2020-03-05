522989f61ab5191f8b7c64aa7cf1d60cce0d1c0b
Brandon Douglas Johnson <brandojd93@gmail.com>
2020-03-05 13:34:17 -0600

    The map is better now.

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,7 +1,14 @@
 class MapsController < ApplicationController
   def show
-    @tutor_profiles = TutorProfile.geocoded
-    @center = [22,-44]
+    if params[:location].blank?
+      redirect_to root_path
+      return
+    end
+
+    results = Geocoder.search(params[:location])
+    @center = results.first.coordinates
+
+    @tutor_profiles = TutorProfile.near(@center, 50)
     @markers = @tutor_profiles.map do |tutor_profile|
       {
         lat: tutor_profile.latitude,

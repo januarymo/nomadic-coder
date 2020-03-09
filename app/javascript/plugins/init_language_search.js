a1b339041f8ba61e85abd38702dadd0825cb745f
Brandon Douglas Johnson <brandojd93@gmail.com>
2020-03-09 17:45:15 -0600

    Finished language search

--- app/javascript/plugins/init_language_search.js
+++ app/javascript/plugins/init_language_search.js
@@ -2,7 +2,9 @@ import 'select2';
 import 'select2/dist/css/select2.css';
 
 const initLanguageSearch = () => {
-  $('.js-language-search').select2();
+  $('.js-language-search').select2({
+    placeholder: "Language"
+  });
 }
 
 export { initLanguageSearch }
